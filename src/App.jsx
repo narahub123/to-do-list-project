26020af81068175cd4fa6f65e692d612cf4e2937
nara <parknara0213@gmail.com>
2024-02-19 13:49:15 +0900

    240219 Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,9 @@
+import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <>
       <main>
-        <aside>
-          <section>
-            <h3>Your Weekly Plans</h3>
-            <ul>
-              <li>
-                2024
-                <ul>
-                  <li>
-                    1월
-                    <ul>
-                      <li>week1</li>
-                      <li>week2</li>
-                      <li>week3</li>
-                      <li>week4</li>
-                      <li>week5</li>
-                    </ul>
-                  </li>
-                  <li>
-                    2월
-                    <ul>
-                      <li>week1</li>
-                      <li>week2</li>
-                      <li>week3</li>
-                      <li>week4</li>
-                      <li>week5</li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </section>
-          <section>
-            <h3>Other User's Plan</h3>
-            <ol>
-              <li>User1</li>
-              <li>User2</li>
-              <li>User3</li>
-              <li>User4</li>
-            </ol>
-          </section>
-        </aside>
+        <Sidebar />
       </main>
     </>
   );
