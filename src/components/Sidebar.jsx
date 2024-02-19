bc50135d521f5bff0f437fa4b2e59a3f1009aa15
nara <parknara0213@gmail.com>
2024-02-19 18:24:52 +0900

    240219 calculate week number

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,17 @@
 import Button from "./Button";
 
 export default function Sidebar({ onStartAddProject, plans }) {
+  // calculate week number of the date
+  function handleWeekNumber(date) {
+    const dataObject = new Date(date);
+    const startOfYear = new Date(dataObject.getFullYear(), 0, 0);
+    const diff = dataObject - startOfYear;
+    const oneWeek = 1000 * 60 * 60 * 24 * 7;
+    const weekNumber = Math.floor(diff / oneWeek) + 1;
+    // console.log(weekNumber);
+    return weekNumber;
+  }
+
   return (
     <aside className="w-1/3 px-8 py-6 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <section>
@@ -8,13 +19,16 @@ export default function Sidebar({ onStartAddProject, plans }) {
           Your Weekly Plans
         </h3>
         <ul className="mb-8">
-          {plans.map((plan) => (
-            <li key={plan.planId}>
-              <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
-                {plan.subject}
-              </button>
-            </li>
-          ))}
+          {plans.map((plan) => {
+            let weekNumber = handleWeekNumber(plan.from);
+            return (
+              <li key={plan.planId}>
+                <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+                  week{weekNumber}
+                </button>
+              </li>
+            );
+          })}
         </ul>
         <Button onClick={onStartAddProject}>+ Add Plan</Button>
       </section>
