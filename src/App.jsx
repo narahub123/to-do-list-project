a40a814155da2ab3d74ba675de9247cc66323e08
nara <parknara0213@gmail.com>
2024-02-19 13:42:36 +0900

    240219 aside structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
 function App() {
-  return <>To do lists</>;
+  return (
+    <>
+      <main>
+        <aside>
+          <section>
+            <h3>Your Weekly Plans</h3>
+            <ul>
+              <li>
+                2024
+                <ul>
+                  <li>
+                    1월
+                    <ul>
+                      <li>week1</li>
+                      <li>week2</li>
+                      <li>week3</li>
+                      <li>week4</li>
+                      <li>week5</li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>Other User's Plan</h3>
+            <ol>
+              <li>User1</li>
+              <li>User2</li>
+              <li>User3</li>
+              <li>User4</li>
+            </ol>
+          </section>
+        </aside>
+      </main>
+    </>
+  );
 }
 
 export default App;
