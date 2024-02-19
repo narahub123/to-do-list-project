6dda03e91894757b96becdf88ba7fa9927cb3799
nara <parknara0213@gmail.com>
2024-02-19 13:59:08 +0900

    240219 split sidebar component and add button element

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,35 +1,11 @@
+import WeeklyPlans from "./WeeklyPlans";
 export default function Sidebar() {
   return (
     <aside>
       <section>
         <h3>Your Weekly Plans</h3>
-        <ul>
-          <li>
-            2024
-            <ul>
-              <li>
-                1월
-                <ul>
-                  <li>week1</li>
-                  <li>week2</li>
-                  <li>week3</li>
-                  <li>week4</li>
-                  <li>week5</li>
-                </ul>
-              </li>
-              <li>
-                2월
-                <ul>
-                  <li>week1</li>
-                  <li>week2</li>
-                  <li>week3</li>
-                  <li>week4</li>
-                  <li>week5</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <button>+ Add Plan</button>
+        <WeeklyPlans />
       </section>
       <section>
         <h3>Other User's Plan</h3>
