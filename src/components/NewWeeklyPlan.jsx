6ab618123e29eeac75b762a4e1aa244fe4862b8d
nara <parknara0213@gmail.com>
2024-02-19 15:20:30 +0900

    240219 styling newweeklyplan

--- src/components/NewWeeklyPlan.jsx
+++ src/components/NewWeeklyPlan.jsx
@@ -1,31 +1,25 @@
+import Input from "./Input";
+
 export default function NewWeeklyPlan() {
   return (
-    <div>
+    <div className="w-[35rem] mt-16">
       <div>
         <p>Weekly Plan</p>
-        <p>
-          <label>From </label>
-          <input type="date" />
-        </p>
-        <p>
-          <label>To </label>
-          <input type="date" />
-        </p>
-        <p>
-          <label>Subject </label>
-          <input type="text" />
-        </p>
-        <p>
-          <label>Description </label>
-          <input type="text" />
-        </p>
+        <Input label="From" type="date" />
+        <Input label="To" type="date" />
+        <Input label="Subject" type="text" />
+        <Input label="Description" type="text" />
       </div>
-      <menu>
+      <menu className="flex item-center justify-end gap-4 my-4">
         <li>
-          <button>Cancel</button>
+          <button className="px-6 py-2 text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
         </li>
         <li>
-          <button>Save</button>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+            Save
+          </button>
         </li>
       </menu>
     </div>
