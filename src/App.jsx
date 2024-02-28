61afc61dabe52d99f04082374f2c6f65f15bbc31
nara <parknara0213@gmail.com>
2024-02-28 10:23:21 +0900

    240228 create routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import Sidebar from "./components/sidebar/Sidebar";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MonthlyDashboard from "./pages/MonlyDashboard";
+import WeeklyDashboard from "./pages/WeeklyDashboard";
+import DailyDashboard from "./pages/DailyDashboard";
+import Settings from "./pages/Settings";
 function App() {
   return (
-    <>
-      <Sidebar />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MonthlyDashboard />} />
+        <Route path="/monthly" element={<MonthlyDashboard />} />
+        <Route path="/weekly" element={<WeeklyDashboard />} />
+        <Route path="/daily" element={<DailyDashboard />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,22 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MonthlyDashboard from "../../pages/MonlyDashboard";
-import WeeklyDashboard from "../../pages/WeeklyDashboard";
-import DailyDashboard from "../../pages/DailyDashboard";
-import Settings from "../../pages/Settings";
 
 const Sidebar = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MonthlyDashboard />} />
-        <Route path="/monthly" element={<MonthlyDashboard />} />
-        <Route path="/weekly" element={<WeeklyDashboard />} />
-        <Route path="/daily" element={<DailyDashboard />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>This is Sidebar</div>;
 };
 
 export default Sidebar;
